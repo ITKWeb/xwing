3e9f2b8ccf272420e1ed2261ca472af7db438ef7
jennifer251 <Benjy251>
2013-03-15 16:01:18 +0100

    model update

--- xwingweb/xwing/templates/views.py
+++ xwingweb/xwing/templates/views.py
@@ -1,6 +1,6 @@
 
 from django.shortcuts import render_to_response
-from xwingweb.models import Boite
+from xwingweb.xwing.models import Boite
 def index(request):    
     list_boites = Boite.objects.all()         
 	return render_to_response('index.html', { 'list_boites': list_boites })
